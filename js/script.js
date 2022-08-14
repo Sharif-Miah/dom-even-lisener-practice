11d91b5c8eee0c77b1190efd2dc0f6834f2a7b1e
farhan sharif <farhansharif1410@gmail.com>
2022-08-13 22:52:17 -0230

    background color added another option

--- js/script.js
+++ js/script.js
@@ -7,10 +7,19 @@ function backgraundYellow() {
 }
 
 // Another Option add to background Color
+// option 3:
 
 const makeBlue = document.getElementById("make-blue");
 makeBlue.onclick = makeBlueColor
 
 function makeBlueColor() {
     document.body.style.backgroundColor = 'blue'
+}
+
+// Option 3 another
+
+const purpleColor = document.getElementById("make-purple");
+
+purpleColor.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple'
 }
