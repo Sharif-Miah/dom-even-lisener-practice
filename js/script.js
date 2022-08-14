9720775dd3d615678dc48e038c5f398284839222
farhan sharif <farhansharif1410@gmail.com>
2022-08-13 22:42:05 -0230

    added background color in js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function backgraundRed() {
 function backgraundYellow() {
     document.body.style.backgroundColor = 'yellow'
 }
+
+// Another Option add to background Color
+
+const makeBlue = document.getElementById("make-blue");
+makeBlue.onclick = makeBlueColor
+
+function makeBlueColor() {
+    document.body.style.backgroundColor = 'blue'
+}
